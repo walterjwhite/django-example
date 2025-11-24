901cfa96c540d41cb523949cadf6fa4fdbae4503
Walter White <WalterWhite@walterjwhite.com>
2025-11-24 09:40:50 -0500

    default - settings - allowed hosts

--- example_django/settings.py
+++ example_django/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=vaf^#ig5pfhaarjps7m&2s#$(@%m!fg7efwqdi^0ol6^e79u(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1],.render.com,.koyeb.com").split(",")
+ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost,127.0.0.1,[::1],.render.com,.koyeb.app").split(",")
 
 
 # Application definition
